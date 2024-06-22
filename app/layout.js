c487afcfb5632704b1ef2a971b6d56f13f277b0a
Bilal Lodhi <bilallodhi739@gmail.com>
2024-06-22 17:38:31 +0500

    nothing

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-"use client"; // Add this line at the top
+// layout.js
+"use client";
 import React, { useState } from "react";
 import '../styles/globals.css';
 import { CartProvider } from './context/CartContext';
@@ -17,7 +18,7 @@ export default function RootLayout({ children }) {
       <body>
         <CartProvider>
           <header className="fixed top-0 left-0 right-0 z-50 bg-grey shadow-md">
-            <div className="max-w-6x2 flex items-center justify-between mx-auto p-4 md:p-4">
+            <div className="max-w-6xl flex items-center justify-between mx-auto p-4 md:p-4">
               <div className="flex items-center">
                 <Link href="/" legacyBehavior>
                   <div className="text-white font-semibold text-2xl md:text-3xl ml-0">
@@ -26,7 +27,6 @@ export default function RootLayout({ children }) {
                 </Link>
               </div>
               <nav className="hidden text-white md:flex gap-6 text-primary items-center font-semibold text-lg">
-             
                 <Link href="/" legacyBehavior>
                   <div className="hover:underline">Home</div>
                 </Link>
@@ -34,9 +34,8 @@ export default function RootLayout({ children }) {
                   <div className="hover:underline">Shopping</div>
                 </Link>
                 <Link href="/about" legacyBehavior>
-                  <div className="hover:underline">About  </div>
+                  <div className="hover:underline">About</div>
                 </Link>
-
                 <Link href="/cart" legacyBehavior>
                   <div className="flex items-center hover:underline">
                     <FaShoppingCart className="mr-2" /> Cart
@@ -55,20 +54,12 @@ export default function RootLayout({ children }) {
                   Home
                 </Link>
                 <Link href="/shop" legacyBehavior>
-                  <div className="py-2 text-center"onClick={toggleMenu}>Shopping</div>
+                  <div className="py-2 text-center" onClick={toggleMenu}>Shopping</div>
                 </Link>
-                <Link
-                  href="/about"
-                  className="py-2 text-center"
-                  onClick={toggleMenu}
-                  legacyBehavior>
+                <Link href="/about" className="py-2 text-center" onClick={toggleMenu} legacyBehavior>
                   About us
                 </Link>
-                <Link
-                  href="/cart"
-                  className="py-2 text-center flex items-center justify-center"
-                  onClick={toggleMenu}
-                  legacyBehavior>
+                <Link href="/cart" className="py-2 text-center flex items-center justify-center" onClick={toggleMenu} legacyBehavior>
                   <FaShoppingCart className="mr-2" /> Cart
                 </Link>
               </nav>
@@ -79,7 +70,7 @@ export default function RootLayout({ children }) {
               }
             `}</style>
           </header>
-          <main className="pt-16">{/* Add padding-top to prevent overlap */}
+          <main className="pt-16">
             {children}
           </main>
         </CartProvider>
